63703ab5308144146b88c47212ce0e5227400dd6
bzantium <ryumin93@gmail.com>
2018-12-12 19:54:23 +0900

    update

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ import torch
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader, Dataset
 from params import param
-from gensim.models.keyedvectors import KeyedVectors
 
 
 def read_data(file_path_dataset):
@@ -55,25 +54,6 @@ def save_model(net, filename):
                os.path.join(param.model_root, filename))
     print("save pretrained model to: {}".format(os.path.join(param.model_root,
                                                              filename)))
-
-
-def load_pretrained(path, reverse_vocab):
-    print("loading word2vec...")
-    word_vectors = KeyedVectors.load_word2vec_format(path, binary=True)
-    pretrain_embed = list()
-
-    pretrain_embed.append(np.zeros(300).astype("float32")) # for <PAD> token
-    pretrain_embed.append(np.random.uniform(-0.01, 0.01, 300).astype("float32")) # for <UNK> token
-    for i in range(len(reverse_vocab)-2):
-        word = reverse_vocab[i+2]
-        if word in word_vectors.vocab:
-            pretrain_embed.append(word_vectors.word_vec(word))
-        else:
-            pretrain_embed.append(np.random.uniform(-0.01, 0.01, 300).astype("float32"))
-
-    pretrain_embed = np.array(pretrain_embed)
-    print("word2vec successfully loaded")
-    return pretrain_embed
 
 
 def get_data_loader(sequences, labels, maxlen=None):
